4470b27b69670a811252dfb14f856c8bc6153859
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2024-08-23 13:19:44 +0530

    Updated responsive design

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -16,19 +16,3 @@ tailwind.config = {
       menu.classList.toggle("hidden");
   });
 
-
-//   slider js
-        const slider = document.querySelector('.slider-content');
-        const slides = document.querySelectorAll('.slider-item');
-        let currentIndex = 0;
-        const totalSlides = slides.length;
-
-        function showNextSlide() {
-            currentIndex++;
-            if (currentIndex >= totalSlides) {
-                currentIndex = 0;
-            }
-            slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-        }
-
-        setInterval(showNextSlide, 3000); // Change slide every 3 seconds
