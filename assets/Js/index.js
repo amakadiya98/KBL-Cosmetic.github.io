afd1310e3d8fa69dfb47619ef09e7855e911d796
amakadiya98 <amakadiya98@gmail.com>
2024-08-29 18:59:30 +0530

    blog and solution page changes

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -16,3 +16,40 @@ tailwind.config = {
       menu.classList.toggle("hidden");
   });
 
+// tab part 
+
+ const links = document.querySelectorAll('.tab-link');
+      
+ links.forEach(link => {
+   link.addEventListener('click', function () {
+     links.forEach(l => l.className = 'tab-link py-4 uppercase text-[14px] text-[#0000003b] mx-10');
+     this.className += ` ${this.getAttribute('data-active')}`;
+   });
+ });
+
+//  smooth scroll 
+ function smoothScroll(event, targetId) {
+  event.preventDefault();
+
+  const targetElement = document.querySelector(targetId);
+  if (!targetElement) return;
+
+  const startPosition = window.pageYOffset;
+  const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1000; 
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const progress = Math.min(timeElapsed / duration, 1); 
+    window.scrollTo(0, startPosition + distance * progress);
+
+    if (timeElapsed < duration) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  requestAnimationFrame(animation);
+}
